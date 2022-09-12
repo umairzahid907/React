2520049437df20c13ce146288b8e4fa201221eda
Muhammad Umair Zahid <umair.zahid@devsinc.com>
2022-09-12 11:11:44 +0500

    fixed some issues

--- src/components/ShowPost.js
+++ src/components/ShowPost.js
@@ -9,18 +9,19 @@ export default function ShowPost(){
   const [comment, setComment] = useState([]);
   const { id } = useParams();
   const url = "http://localhost:3001/posts/" + id;
+
   useEffect(() => {
     axios.get(url)
     .then((res) => {
       setPost(res.data.posts);
       setComment(res.data.comments);
     })
-  });
+  }, []);
 
   return(
     <>
       <div className='d-flex justify-content-between pt-3 pb-3'>
-        <Link to="/" className='btn btn-dark'>{'Back'}</Link>
+        <Link to='' className='btn btn-dark'>{'Back'}</Link>
       </div>
 
       <h5 className='display-3 text-center head'>{ post.title }</h5>
@@ -33,23 +34,23 @@ export default function ShowPost(){
       </div>
 
       <div id='like'>
-        <Link to="/" className='btn btn-primary'>{'Like'}</Link>
+        <Link to='' className='btn btn-primary'>{'Like'}</Link>
       </div>
 
-      <Link to="/" className='btn btn-warning'>{'Report this post'}</Link>
-      <Link to="/" className='btn btn-primary'>{'Give Suggestion'}</Link>
+      <Link to='' className='btn btn-warning'>{'Report this post'}</Link>
+      <Link to='' className='btn btn-primary'>{'Give Suggestion'}</Link>
 
       <h2>Comments</h2>
       {
-      comment.map((c) => {
-        <div className='card w-75'>
-          <div className='card-body'>
-            <h5 className='card-title'>{ c.user_email}</h5>
-            <p className='card-text'>{ c.body }</p>
-            <Link to="/" className='btn btn-dark'>{'Delete'}</Link>
+        comment.map(c =>
+          <div key={c.id} className='card w-75'>
+            <div className='card-body'>
+              <h5 className='card-title'>{ c.user_email}</h5>
+              <p className='card-text'>{ c.body }</p>
+              <Link to='' className='btn btn-dark'>{'Delete'}</Link>
+            </div>
           </div>
-        </div>
-      })
+        )
       }
     </>
   );
