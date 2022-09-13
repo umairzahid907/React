516b09635b944aa7e095aa896e1f462e19537e2e
Muhammad Umair Zahid <umair.zahid@devsinc.com>
2022-09-13 14:09:49 +0500

    handle error in ShowPost.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import React from 'react';
-import './App.css';
-import Post from './components/Posts.js';
-import ShowPost from './components/ShowPost';
-import { BrowserRouter, Route, Routes} from 'react-router-dom';
+import React from 'react'
+import './App.css'
+import Post from './components/Posts.js'
+import ShowPost from './components/ShowPost'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
-function App(){
+function App () {
   return (
     <BrowserRouter>
       <div className='App'>
         <Routes>
-          <Route path="/Posts" element={<Post />} />
-          <Route path="/" element={<Post />} />
-          <Route path="/Posts/:id" element={<ShowPost />} />
+          <Route path='/Posts' element={<Post />} />
+          <Route path='/' element={<Post />} />
+          <Route path='/Posts/:id' element={<ShowPost />} />
         </Routes>
       </div>
     </BrowserRouter>
-  );
+  )
 }
 
-export default App;
+export default App
